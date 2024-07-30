cf24f5b913e84661fb12fc7d9cf9a5c29898230a
SebastienGuillemin <sebastien.guillemin11@gmail.com>
2024-07-30 22:37:12 +0200

    Error when running tests from the 'forexchangecommunication/binance' adapter. Maybe check the import conflicts

---
+++ arbibot-infra/src/main/java/com/arbibot/infra/adapter/fortriangulararbitraging/web/configuration/DependencyConfig.java
@@ -0,0 +1,22 @@
+// package com.arbibot.infra.adapter.fortriangulararbitraging.web.configuration;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.context.annotation.Configuration;
+
+// import com.arbibot.core.ports.output.ForExchangeCommunication;
+// import com.arbibot.core.usecases.arbitrage.TriangularArbitrage;
+
+// @Configuration
+// // @ComponentScan(basePackages = "com.arbibot")
+// public class DependencyConfig {
+//     @Autowired
+//     private ForExchangeCommunication forExchangeCommunication;
+
+//     @Bean
+//     public TriangularArbitrage triangularArbitrage() {
+//         return new TriangularArbitrage(forExchangeCommunication);
+//     }
+    
+// }
