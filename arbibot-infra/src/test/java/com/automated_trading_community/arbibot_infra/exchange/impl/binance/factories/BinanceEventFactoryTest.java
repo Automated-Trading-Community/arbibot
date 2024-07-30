e3a17f8333de849d0ef33da75a6e98172b5e7a75
SChoumiloff <114927920+SChoumiloff@users.noreply.github.com>
2024-07-30 12:08:55 +0200

    all tests done except for binance class

--- arbibot-infra/src/test/java/com/automated_trading_community/arbibot_infra/exchange/impl/binance/factories/BinanceEventFactoryTest.java
+++ arbibot-infra/src/test/java/com/automated_trading_community/arbibot_infra/exchange/impl/binance/factories/BinanceEventFactoryTest.java
@@ -3,26 +3,30 @@ package com.automated_trading_community.arbibot_infra.exchange.impl.binance.fact
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.StreamUtils;
 
 import com.automated_trading_community.arbibot_infra.exchange.impl.binance.models.ExecutionReport;
 import com.automated_trading_community.arbibot_infra.exchange.impl.binance.models.PositionBinance;
 import com.automated_trading_community.arbibot_infra.exchange.impl.binance.models.UserDataEventBinance;
 
+import org.springframework.core.io.Resource;
+
+import org.springframework.beans.factory.annotation.Value;
+
+@SpringBootTest
 class BinanceEventFactoryTest {
 
     @Test
-    void testCreateExecutionReportEvent() throws IOException {
-        String json = loadJsonFromFile(
-                "/Users/sachachoumiloff/Desktop/automated-trading-community/arbibot/arbibot-infra/src/test/ressources/files/payload/binance/executionOrder1Buy.json");
+    void testCreateExecutionReportEvent(@Value("__files/payload/binance/executionOrder1Buy.json") Resource exResource)
+            throws IOException {
+        String json = StreamUtils.copyToString(exResource.getInputStream(), StandardCharsets.UTF_8);
         UserDataEventBinance event = BinanceEventFactory.createEvent(json);
 
         assertNotNull(event);
@@ -33,18 +37,21 @@ class BinanceEventFactoryTest {
     }
 
     @Test
-    void testCreatePositionBinanceEvent() throws IOException {
-        String json = loadJsonFromFile(
-                "/Users/sachachoumiloff/Desktop/automated-trading-community/arbibot/arbibot-infra/src/test/ressources/files/payload/binance/position.json");
+    void testCreatePositionBinanceEvent(@Value("__files/payload/binance/position.json") Resource posResource)
+            throws IOException {
+        String json = StreamUtils.copyToString(posResource.getInputStream(), StandardCharsets.UTF_8);
         UserDataEventBinance event = BinanceEventFactory.createEvent(json);
-
+        System.out.println(event.getClass());
         assertNotNull(event);
         assertTrue(event instanceof PositionBinance);
         PositionBinance positionBinance = (PositionBinance) event;
         assertEquals("outboundAccountPosition", positionBinance.getEventType());
     }
 
-    private String loadJsonFromFile(String path) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8);
+    @Test
+    void notKnownEvent(@Value("__files/payload/binance/notKnownEvent.json") Resource notKnow) throws IOException {
+        String json = StreamUtils.copyToString(notKnow.getInputStream(), StandardCharsets.UTF_8);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> BinanceEventFactory.createEvent(json));
+        assertEquals(exception.getMessage(), "Unknown type: notKnownEvent");
     }
 }
---
+++ arbibot-infra/src/test/java/com/automated_trading_community/arbibot_infra/exchange/impl/binance/models/MiniTickerTest.java
@@ -0,0 +1,37 @@
+package com.automated_trading_community.arbibot_infra.exchange.impl.binance.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StreamUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+public class MiniTickerTest {
+    @Test
+    public void testMiniTicker(@Value("__files/payload/binance/miniTickerBinance.json") Resource miniTickerResource)
+            throws IOException {
+
+        String json = StreamUtils.copyToString(miniTickerResource.getInputStream(), StandardCharsets.UTF_8);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        MiniTicker miniTicker = objectMapper.readValue(json, MiniTicker.class);
+
+        assertEquals("miniTicker", miniTicker.getEvent());
+        assertEquals(1622263514064L, miniTicker.getEventTime());
+        assertEquals("BTCUSDT", miniTicker.getSymbol());
+        assertEquals("35000.12", miniTicker.getClosePrice().toPlainString());
+        assertEquals("34000.00", miniTicker.getOpenPrice());
+        assertEquals("35500.00", miniTicker.getHighPrice());
+        assertEquals("33500.00", miniTicker.getLowPrice());
+        assertEquals("12345.67", miniTicker.getVolume());
+        assertEquals("456789.12", miniTicker.getQuoteVolume());
+    }
+}
