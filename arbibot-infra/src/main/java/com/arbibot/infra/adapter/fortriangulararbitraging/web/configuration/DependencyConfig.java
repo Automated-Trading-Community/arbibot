0390fb3fb6e5203a50fbdfc7cf0d5ecf24bd7803
Sébastien GUILLEMIN <sebastien.guillemin11@gmail.com>
2024-07-31 13:19:26 +0200

    TriangularArbitrageController

--- arbibot-infra/src/main/java/com/arbibot/infra/adapter/fortriangulararbitraging/web/configuration/DependencyConfig.java
+++ arbibot-infra/src/main/java/com/arbibot/infra/adapter/fortriangulararbitraging/web/configuration/DependencyConfig.java
@@ -1,22 +1,22 @@
-// package com.arbibot.infra.adapter.fortriangulararbitraging.web.configuration;
+package com.arbibot.infra.adapter.fortriangulararbitraging.web.configuration;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.ComponentScan;
-// import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-// import com.arbibot.core.ports.output.ForExchangeCommunication;
-// import com.arbibot.core.usecases.arbitrage.TriangularArbitrage;
+import com.arbibot.core.ports.output.ForExchangeCommunication;
+import com.arbibot.core.usecases.arbitrage.TriangularArbitrage;
 
-// @Configuration
-// // @ComponentScan(basePackages = "com.arbibot")
-// public class DependencyConfig {
-//     @Autowired
-//     private ForExchangeCommunication forExchangeCommunication;
+@Configuration
+@ComponentScan(basePackages = {"com.arbibot"})
+public class DependencyConfig {
+    @Autowired
+    private ForExchangeCommunication forExchangeCommunication;
 
-//     @Bean
-//     public TriangularArbitrage triangularArbitrage() {
-//         return new TriangularArbitrage(forExchangeCommunication);
-//     }
+    @Bean
+    public TriangularArbitrage triangularArbitrage() {
+        return new TriangularArbitrage(forExchangeCommunication);
+    }
     
-// }
+}
