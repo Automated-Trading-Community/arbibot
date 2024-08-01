d648ce07911bd267528b43f4c733a4ce1b13b938
SChoumiloff <114927920+SChoumiloff@users.noreply.github.com>
2024-08-01 15:34:46 +0200

    javadoc
    - javadoc pour les exceptions

--- arbibot-infra/src/main/java/com/arbibot/infra/adapter/forexchangecommunication/binance/exceptions/BinanceWrongApiKeys.java
+++ arbibot-infra/src/main/java/com/arbibot/infra/adapter/forexchangecommunication/binance/exceptions/BinanceWrongApiKeys.java
@@ -1,8 +1,20 @@
 package com.arbibot.infra.adapter.forexchangecommunication.binance.exceptions;
 
+/**
+ * A {@code BinanceWrongApiKeys} is thrown when the Binance API keys are wrong
+ * or missing.
+ *
+ * @since 1.0
+ * @author SChoumiloff SGuillemin
+ */
 public class BinanceWrongApiKeys extends Exception {
 
-    
+    /**
+     * Constructs a new {@code BinanceWrongApiKeys} with the specified
+     * detail message.
+     *
+     * @param errorMessage the detail message describing the apiKey error
+     */
     public BinanceWrongApiKeys(String errorMessage) {
         super(errorMessage);
     }
--- arbibot-infra/src/main/java/com/arbibot/infra/adapter/forexchangecommunication/binance/exceptions/BinanceWrongPermissions.java
+++
@@ -1,7 +0,0 @@
-package com.arbibot.infra.adapter.forexchangecommunication.binance.exceptions;
-
-public class BinanceWrongPermissions extends Exception {
-    public BinanceWrongPermissions(String errorMessage) {
-        super(errorMessage);
-    }
-}
