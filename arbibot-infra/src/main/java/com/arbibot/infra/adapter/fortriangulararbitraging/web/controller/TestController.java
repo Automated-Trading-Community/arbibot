4dfc1edaa96393875af6d3238505e1c012ebf93e
SebastienGuillemin <sebastien.guillemin11@gmail.com>
2024-08-05 22:50:33 +0200

    Get price function in TestController

--- arbibot-infra/src/main/java/com/arbibot/infra/adapter/fortriangulararbitraging/web/controller/TestController.java
+++ arbibot-infra/src/main/java/com/arbibot/infra/adapter/fortriangulararbitraging/web/controller/TestController.java
@@ -1,7 +1,13 @@
 package com.arbibot.infra.adapter.fortriangulararbitraging.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.arbibot.core.entities.Asset;
+import com.arbibot.core.entities.Pair;
+import com.arbibot.infra.adapter.forexchangecommunication.binance.Binance;
+
 
 /**
  * TODO : controller to test if the web server is up.
@@ -19,5 +25,13 @@ public class TestController {
     @GetMapping("/ping")
     public String getMethodName() {
         return "Pong";
+    }
+
+    @GetMapping("/price")
+    public String getProceForPair(@RequestParam String asset1, @RequestParam String asset2, Binance binance) {
+        Pair pair = new Pair(new Asset(asset1), new Asset(asset2));
+        binance.getPriceForPair(pair);
+
+        return pair.getPrice().toString();
     }
 }
